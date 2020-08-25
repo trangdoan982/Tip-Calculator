30489834b6139c8d4ea6233d6a0cec70b72b9332
Trang Doan <doantranghp2000@gmail.com>
2020-08-25 17:36:37 +0700

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Trang Doan on 8/25/20.
+//  Copyright © 2020 Trang Doan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
